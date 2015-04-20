2fb5a348dbd9fa415ee436ea6179f684b7628ef7
Larry Boniface <Lawrence.Boniface@gmail.com>
2015-04-20 02:24:25 -0600

    working version

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,18 +4,23 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item the given array.
 
-  //Code Here
 
+function first(arr) {
+  return arr([0]);
+}
+first();
 
 //Next problem
-
 
 
 var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item the given array.
 
 
-  //Code Here
+function last(arr) {
+  return arr[arr.length-1];
+}
+last();
 
 
 //Next Problem
@@ -24,17 +29,21 @@ var arr = [40,50,60];
 var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 //Create a function named 'looper' that is given family as it's only argument, loops through the given array, and alerts every item in the array.
 
-  //Code Here
-
+function looper(arr) {
+    for (var i=0; i<arr.length; i++) {
+        alert(family[i]);
+    }
+}
+looper(family);
 
 //Next problem
 
-
-
-var letters = ['A', 'B', 'C', 'D', 'E'];
-//Write a function called reversedLooper that is given letters as it's only argument and loops through the given array backwards alerting every item in the array starting at the end.
-
-  //Code Here
+function reversedLooper(arr) {
+  for (var i=arr.length - 1; i>=0; i--) {
+    alert(arr[i]);
+  }
+}
+reversedLooper(letters);
 
 
 //Next Problem
@@ -43,21 +52,37 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 var nums = [1,2,3,6,22,98,45,23,22,12];
 //Write a function named evenFinder that is given nums as it's only argument and removes all values that aren't even from the given array.
 
-  //Code Here
-
+function evenFinder(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        while (arr[i] % 2) {
+            arr.splice(i, 1);
+        }
+    }
+  }
+console.log(evenFinder(nums);
 
 //Next problem
 
 
-var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
-var evens = [];
-var odds = [];
 //Write a function called divider that is given three arguments, nums, evens, and odds.
 //Have divider return an Array with the first item in the array being the evens array (all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
 
 
+var nums = [1,2,34,54,55,34,32,11,19,17,54,66,13];
+var evens = [];
+var odds = [];  
 
-  //Code Here
+function divider(nums, evens, odds) {
+  for (var i=0; i<nums.length; i++) {
+    if (nums[i] % 2) {
+      odds.push(nums[i]);
+    } else {
+      evens.push(nums[i]);
+    }
+  }
+      return [evens, odds];
+}
+divider(nums, evens, odds);
 
 
 //Next Problem
@@ -67,20 +92,22 @@ var getRandomArbitrary = function() {
   return Math.floor(Math.random() * (30 - 0) + 0);
 }
 var numbers = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
-//Above you're given a function that will return a random number between 0 and 30, and an array full of numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
+//Above you're given a function that will return a random number between 0 and 30, and an array full or numbers. Your job is to write a function named finder that will get a random number, then loop through the array to see if that random number is in the array. If it is, return true, if it's not, return false
 
+function finder(arr) {
+  return arr.indexOf(getRandomArbitrary()) < 0 ? false : true;
+}
 
 //Next problem
 
-
-
 var str = 'this is my sentence';
+
 //Write a function called reverse that takes is given str as it's only argument and returns that string after it's been reversed
 
-  //Code Here
-
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
 
 //Next Problem
 
@@ -99,15 +126,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   myGroceryList then return the new, updated grocery list.
 */
 
-  //Code Here
-
+function removeItem(arr, str) {
+  return arr.indexOf(str) >= 0 ? (arr.splice(arr.indexOf(str),1), arr) : (str + ': is not in your list');
+}
+removeItem(myGroceryList, 'chips');
+  
 //removeItem('chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem('Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
 
 
-
-//Next Problem
-
+function addItem(arr, str) {
+  return arr.indexOf(str) < 0 ? (arr.push(str), arr) : (str + ': is already in your list');
+}
+addItem(myGroceryList, 'Jerky');
 
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
